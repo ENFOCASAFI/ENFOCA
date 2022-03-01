b14129a92e70fbc438102b57ce841e8d225019ff
ENFOCASAFI <97800770+ENFOCASAFI@users.noreply.github.com>
2022-02-28 21:32:33 -0500

    Add files via upload

---
+++ alvpercon_fcb/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+	'name': 'Campo Booleano',
+	'version': '14.0.1.0.0',
+	'category': 'Extra Tools',
+	'summary': 'Camp booleano para pagos de caja chica en facturas',
+	'author': 'Alvpercon',
+	'website': 'https://www.alvpercon.com',
+	'depends': ['account'],
+	'data': [
+		
+		'views/account_move_view.xml',
+	],
+	'installable': True,
+	'sequence': 1,
+}
