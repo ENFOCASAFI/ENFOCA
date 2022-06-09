24f3fa38dde12f6f343afb73307ff863216cebee
ENFOCASAFI <97800770+ENFOCASAFI@users.noreply.github.com>
2022-06-09 16:30:20 -0500

    Add files via upload

---
+++ solse_pe_ple/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from . import account_move
+from . import account_analytic
+from . import res_partner
+from . import res_bank
+from . import account_payment
+from . import ple_report
+from . import ple_report_05
+from . import ple_report_06
+from . import ple_report_08
+from . import ple_report_14
