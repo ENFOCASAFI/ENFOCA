09b508f5d2ec87593e68949bac20fc635d44a2f4
Develogers <code.repository@develogers.com>
2022-07-22 17:10:38 -0500

    Update account_payment_register.py

--- solse_pe_accountant/wizard/account_payment_register.py
+++ solse_pe_accountant/wizard/account_payment_register.py
@@ -41,11 +41,11 @@ class AccountPaymentRegister(models.TransientModel):
 		factura = self.line_ids[0].move_id
 		self.payment_difference_handling = "open"
                 if self.es_detraccion_retencion:
-                        self.currency_id = self.env.ref("base.PEN")
-			self.amount = factura.monto_detraccion + factura.monto_retencion
+                    self.currency_id = self.env.ref("base.PEN")
+	            self.amount = factura.monto_detraccion + factura.monto_retencion
 		else:
-                        self.currency_id = factura.currency_id
-		        self.amount = factura.amount_currency - factura.monto_detraccion_base - factura.monto_retencion_base
+                    self.currency_id = factura.currency_id
+		    self.amount = factura.amount_currency - factura.monto_detraccion_base - factura.monto_retencion_base
 
 	def _create_payment_vals_from_wizard(self):
 		payment_vals = super(AccountPaymentRegister, self)._create_payment_vals_from_wizard()
