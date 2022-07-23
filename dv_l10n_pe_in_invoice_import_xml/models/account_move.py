72ff12e23e8adc1d53ba40fb0e28f4271cdeb6ec
Develogers <code.repository@develogers.com>
2022-07-23 13:18:42 -0500

    Update account_move.py

--- dv_l10n_pe_in_invoice_import_xml/models/account_move.py
+++ dv_l10n_pe_in_invoice_import_xml/models/account_move.py
@@ -1,7 +1,9 @@
-from odoo import models, fields
+from odoo import models, fields, _, api
+from odoo.exceptions import UserError
 import logging
 
 _logger = logging.getLogger(__name__)
+
 
 class import_xml(models.Model):
     _inherit = 'account.move'
@@ -12,7 +14,7 @@ class import_xml(models.Model):
     data_xml = fields.Binary(string="XML")
     datas_fname_pdf = fields.Char("Nombre pdf")
     data_pdf = fields.Binary(string="PDF")
-    
+
     @api.constrains('account_id', 'journal_id')
     def _check_constrains_account_id_journal_id(self):
         for line in self.filtered(lambda x: x.display_type not in ('line_section', 'line_note')):
@@ -20,7 +22,8 @@ class import_xml(models.Model):
             journal = line.move_id.journal_id
 
             if account.deprecated:
-                raise UserError(_('The account %s (%s) is deprecated.') % (account.name, account.code))
+                raise UserError(_('The account %s (%s) is deprecated.') % (
+                    account.name, account.code))
 
             account_currency = account.currency_id
             _logger.info(account)
@@ -29,15 +32,18 @@ class import_xml(models.Model):
             _logger.info(line.company_currency_id)
             _logger.info(line.currency_id)
             if account_currency and account_currency != line.company_currency_id and account_currency != line.currency_id:
-                raise UserError(_('The account selected on your journal entry forces to provide a secondary currency. You should remove the secondary currency on the account.'))
+                raise UserError(
+                    _('The account selected on your journal entry forces to provide a secondary currency. You should remove the secondary currency on the account.'))
 
             if account.allowed_journal_ids and journal not in account.allowed_journal_ids:
-                raise UserError(_('You cannot use this account (%s) in this journal, check the field \'Allowed Journals\' on the related account.', account.display_name))
+                raise UserError(
+                    _('You cannot use this account (%s) in this journal, check the field \'Allowed Journals\' on the related account.', account.display_name))
 
             failed_check = False
             if (journal.type_control_ids - journal.default_account_id.user_type_id) or journal.account_control_ids:
                 failed_check = True
                 if journal.type_control_ids:
-                    failed_check = account.user_type_id not in (journal.type_control_ids - journal.default_account_id.user_type_id)
+                    failed_check = account.user_type_id not in (
+                        journal.type_control_ids - journal.default_account_id.user_type_id)
                 if failed_check and journal.account_control_ids:
                     failed_check = account not in journal.account_control_ids
