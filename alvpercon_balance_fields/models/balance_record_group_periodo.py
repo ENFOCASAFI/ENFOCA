2d05d6033bcfbf91e860b1b8c4ec732639fd04e4
ENFOCASAFI <97800770+ENFOCASAFI@users.noreply.github.com>
2024-03-21 19:19:19 -0500

    Add files via upload

---
+++ alvpercon_balance_fields/models/balance_record_group_periodo.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo import tools
+
+class BalanceRecordGroupPeriodo(models.Model):
+	_name = 'balance.record.group.periodo'
+	_auto = False
+	_description = 'Campos para reporte de balance por periodos'
+
+	account = fields.Char('Cuenta Contables')
+	name = fields.Char('Descripción')
+	account_type = fields.Char('tipo de cuenta')
+	account_digit = fields.Char('Digito de cuenta')
+	account_digitd = fields.Char('Digito de la cuenta')
+	credit_amount = fields.Float('Credito')
+	debit_amount = fields.Float('Debito')
+	deudor = fields.Float('Deudor')
+	acreedor = fields.Float('Acreedor')
+	activo = fields.Float('Activo')
+	pasivo = fields.Float('Pasivo')
+	perdida = fields.Float('Pérdida_N')
+	ganancia = fields.Float('Ganancia_N')
+	perdidan = fields.Float('Pérdida_F')
+	ganancian = fields.Float('Ganancia_F')
+	mes = fields.Integer('Mes')
+	mesl = fields.Char('Nombre del Mes')
+	ano = fields.Integer('Año')
+
+	def init(self):
+		tools.drop_view_if_exists(self._cr, 'balance_record_group_periodo')
+		self._cr.execute("""
+            CREATE OR REPLACE VIEW balance_record_group_periodo AS (
+              
+				SELECT
+                    row_number() OVER () AS id,
+					line.account,
+					line.name,
+					line.account_type,
+					line.account_digit,
+					line.account_digitd,
+                    line.credit_amount,
+					line.debit_amount,
+                    line.deudor,
+					line.acreedor,
+					line.activo,
+					line.pasivo,
+					line.perdida,
+					line.ganancia,
+					line.perdidan,
+					line.ganancian,
+	 				line.mes,
+	 				line.mesl,
+	 				line.ano
+					      
+				FROM (                        
+						SELECT 
+							b.code as account,
+							b.name as name,
+							b.internal_group as account_type,
+							substring(b.code, 1, 1) as account_digit,
+							CASE WHEN substring(b.code, 1, 2) = '79' THEN substring(b.code, 1, 2) ELSE substring(b.code, 1, 1) END as account_digitd,
+							sum(a.credit) as credit_amount,
+							sum(a.debit) as debit_amount,							
+							CASE WHEN sum(a.debit)-sum(a.credit) > 0 THEN sum(a.debit)-sum(a.credit) ELSE 0 END as deudor,
+							CASE WHEN sum(a.debit)-sum(a.credit) < 0 THEN (sum(a.debit)-sum(a.credit))*(-1) ELSE 0 END as acreedor,
+							CASE WHEN substring(b.code, 1, 1) in ('1','2','3','4','5') THEN (CASE WHEN sum(a.debit)-sum(a.credit) > 0 THEN sum(a.debit)-sum(a.credit) ELSE 0 END) ELSE 0 END as activo,
+							CASE WHEN substring(b.code, 1, 1) in ('1','2','3','4','5') THEN (CASE WHEN sum(a.debit)-sum(a.credit) < 0 THEN (sum(a.debit)-sum(a.credit))*(-1) ELSE 0 END) ELSE 0 END as pasivo,
+							CASE WHEN (CASE WHEN substring(b.code, 1, 2) = '79' THEN substring(b.code, 1, 2) ELSE substring(b.code, 1, 1) END) in ('6','7','8') THEN (CASE WHEN sum(a.debit)-sum(a.credit) > 0 THEN sum(a.debit)-sum(a.credit) ELSE 0 END) ELSE 0 END as perdida,
+							CASE WHEN (CASE WHEN substring(b.code, 1, 2) = '79' THEN substring(b.code, 1, 2) ELSE substring(b.code, 1, 1) END) in ('7') THEN (CASE WHEN sum(a.debit)-sum(a.credit) < 0 THEN (sum(a.debit)-sum(a.credit))*(-1) ELSE 0 END) ELSE 0 END as ganancia,
+							CASE WHEN (CASE WHEN substring(b.code, 1, 2) = '79' THEN substring(b.code, 1, 2) ELSE substring(b.code, 1, 1) END) in ('8','9') THEN (CASE WHEN sum(a.debit)-sum(a.credit) > 0 THEN sum(a.debit)-sum(a.credit) ELSE 0 END) ELSE 0 END as perdidan,
+							CASE WHEN (CASE WHEN substring(b.code, 1, 2) = '79' THEN substring(b.code, 1, 2) ELSE substring(b.code, 1, 1) END) in ('7') THEN (CASE WHEN sum(a.debit)-sum(a.credit) < 0 THEN (sum(a.debit)-sum(a.credit))*(-1) ELSE 0 END) ELSE 0 END as ganancian,
+							EXTRACT(MONTH FROM a.date) as mes,
+	 						TO_CHAR(a.date,'Mon') as mesl,
+	 						EXTRACT(YEAR FROM a.date) as ano							
+						
+						FROM 
+							account_move_line a 
+						 LEFT JOIN 
+						 	account_account b
+						 ON 
+						 	a.account_id = b.id
+						 WHERE 
+						 	a.account_for_balance_comp= 1 AND a.parent_state = 'posted' 
+						GROUP BY
+							a.account_id,
+							b.code,
+							b.name,
+							b.internal_group,
+							substring(b.code, 1, 1),
+							CASE WHEN substring(b.code, 1, 2) = '79' THEN substring(b.code, 1, 2) ELSE substring(b.code, 1, 1) END,
+							EXTRACT(MONTH FROM a.date),
+	 						TO_CHAR(a.date,'Mon'),
+	 						EXTRACT(YEAR FROM a.date)
+							
+						ORDER BY
+							b.code						
+
+                      ) as line
+			     
+           
+			     
+            )""")
+
+	
---
+++ alvpercon_balance_fields/wizard/balance_date.py
@@ -0,0 +1,245 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
+
+class BalanceDate(models.TransientModel):    
+    _name = "balance.date.report"
+    _description = "Fecha Reporte de Balance"
+
+    fecha_ini=fields.Date("Fecha Inicial" , default=lambda self: fields.Date.context_today(self).replace(year=2022, month=1, day=1))
+    fecha_fin=fields.Date("Fecha Final" , default=fields.Date.context_today)
+    fecha_fin_acum=fields.Date("Fecha Final Acum." , default=fields.Date.context_today)
+    fecha_ini_comp=fields.Date("Fecha Inicial Comp." , default=lambda self: fields.Date.context_today(self).replace(year=2022, month=1, day=1))
+    fecha_fin_comp=fields.Date("Fecha Final Comp." , default=fields.Date.context_today)
+    fecha_fin_comp_acum=fields.Date("Fecha Final Comp. Acum." , default=fields.Date.context_today)
+    journalb_ids = fields.Many2many('account.journal', 'nf_balance_table','balance_ids' ,'journal_ids' , string='Diarios Contables')
+    #is_mensual = fields.Boolean("Mensual")
+    is_mensual = fields.Boolean("Periodo")
+    is_acumulado = fields.Boolean("Acumulado")
+    is_mensual_comp = fields.Boolean("Comparativo")  
+    is_acumulado_comp = fields.Boolean("Acumulado Comparativo")
+    is_saldo_ini = fields.Boolean("Filtro de Diarios Contables")
+
+      
+    
+    def print_report(self):
+        #print("Vamos bien!!!")
+        query ="UPDATE account_move_line SET account_for_balance = 0; "        
+        self.env.cr.execute(query)
+
+        query1 ="UPDATE account_move_line SET account_saldo_ini = ''; "        
+        self.env.cr.execute(query1)
+
+        list_ids = []
+        journalsf_ids = ()
+        for list_b in  self.journalb_ids:
+            list_ids.append(list_b.id)
+
+        journalsf_ids = tuple(list_ids)      
+
+        if self.is_mensual:
+
+            self.env.cr.execute("""UPDATE account_move_line 
+                                    SET account_for_balance = 1
+                                    WHERE date >= %s 
+                                    AND date <= %s;
+            """, [self.fecha_ini, self.fecha_fin])
+
+            # self.env.cr.execute("""UPDATE account_move_line SET account_saldo_ini = account_journal.name
+            #                     FROM account_journal WHERE account_move_line.journal_id = account_journal.id
+            #                     AND account_journal.name like '%APERT%';
+            # """)
+            
+        
+        if self.is_saldo_ini:
+
+            self.env.cr.execute("""UPDATE account_move_line 
+                                    SET account_saldo_ini = 'a'
+                                    WHERE journal_id in %s;
+            """, [journalsf_ids])
+
+            query2 ="UPDATE account_move_line SET account_for_balance = 0 WHERE account_saldo_ini = 'a'; "        
+            self.env.cr.execute(query2)
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Reporte Balance',
+            'res_model': 'balance.record.group',
+            'view_type': 'form',
+            'view_id': False,
+            'view_mode': 'tree,form,pivot',
+            'context': {'search_default_grupo_de_cuentas': 1,},
+            'target': 'current',
+        }
+
+    def print_report_periodo(self):
+        # print("Vamos bien!!!")
+        query ="UPDATE account_move_line SET account_for_balance_comp = 0; "        
+        self.env.cr.execute(query)
+
+        query1 ="UPDATE account_move_line SET account_filtered_comp = ''; "        
+        self.env.cr.execute(query1)
+
+        list_ids = []
+        journalsf_ids = ()
+        for list_b in  self.journalb_ids:
+            list_ids.append(list_b.id)
+
+        journalsf_ids = tuple(list_ids)      
+
+        if self.is_mensual_comp:
+
+            self.env.cr.execute("""UPDATE account_move_line 
+                                    SET account_for_balance_comp = 1
+                                    WHERE date >= %s 
+                                    AND date <= %s;
+            """, [self.fecha_ini, self.fecha_fin])
+            
+        
+            self.env.cr.execute("""UPDATE account_move_line 
+                                    SET account_for_balance_comp = 1
+                                    WHERE date >= %s 
+                                    AND date <= %s;
+            """, [self.fecha_ini_comp, self.fecha_fin_comp])
+            
+        
+        if self.is_saldo_ini:
+
+            self.env.cr.execute("""UPDATE account_move_line 
+                                    SET account_filtered_comp = 'a'
+                                    WHERE journal_id in %s;
+            """, [journalsf_ids])
+
+            query2 ="UPDATE account_move_line SET account_for_balance = 0 WHERE account_filtered_comp = 'a'; "        
+            self.env.cr.execute(query2)
+
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Reporte Balance por Periodo',
+            'res_model': 'balance.record.group.periodo',
+            'view_type': 'form',
+            'view_id': False,
+            'view_mode': 'tree,form,pivot',
+            'context': {'search_default_grupo_de_cuentas': 1,},
+            'target': 'current',
+        }
+
+
+    
+    def print_report_dolares(self):
+        #print("Vamos bien!!!")
+
+        query ="UPDATE account_move_line SET account_for_balance = 0; "        
+        self.env.cr.execute(query)
+
+        query1 ="UPDATE account_move_line SET account_saldo_ini = ''; "        
+        self.env.cr.execute(query1)
+
+        list_ids = []
+        journalsf_ids = ()
+        for list_b in  self.journalb_ids:
+            list_ids.append(list_b.id)
+
+        journalsf_ids = tuple(list_ids)      
+
+        if self.is_mensual:
+
+            self.env.cr.execute("""UPDATE account_move_line 
+                                    SET account_for_balance = 1
+                                    WHERE date >= %s 
+                                    AND date <= %s;
+            """, [self.fecha_ini, self.fecha_fin])
+
+        
+        if self.is_saldo_ini:
+
+            self.env.cr.execute("""UPDATE account_move_line 
+                                    SET account_saldo_ini = 'a'
+                                    WHERE journal_id in %s;
+            """, [journalsf_ids])
+
+            query2 ="UPDATE account_move_line SET account_for_balance = 0 WHERE account_saldo_ini = 'a'; "        
+            self.env.cr.execute(query2)
+
+        # return {
+        #     'type': 'ir.actions.act_window',
+        #     'name': 'Reporte Balance',
+        #     'res_model': 'balance.record.group.dolar',
+        #     'view_type': 'form',
+        #     'view_id': False,
+        #     'view_mode': 'tree,form,pivot',
+        #     'context': {'search_default_grupo_de_cuentas': 1,},
+        #     'target': 'current',
+        # }
+
+
+        #journal_id not in (107,108,109)
+
+    @api.onchange('fecha_fin_comp_acum')
+    def onchange_fecha_fin_comp_acum(self):
+            self.fecha_fin_acum = self.fecha_fin_comp_acum + relativedelta(years=-1)
+
+    @api.onchange('fecha_ini_comp')
+    def onchange_fecha_ini_comp(self):
+            self.fecha_ini = self.fecha_ini_comp + relativedelta(years=-1)
+    
+    @api.onchange('fecha_fin_comp')
+    def onchange_fecha_fin_comp(self):
+            self.fecha_fin = self.fecha_fin_comp + relativedelta(years=-1)
+
+    @api.onchange('is_mensual')
+    def onchange_is_mensual(self):
+        if self.is_mensual :
+            self.is_acumulado = False
+            self.is_mensual_comp = False
+            self.is_acumulado_comp = False
+            self.is_saldo_ini = False
+            self.fecha_fin = fields.Date.context_today(self)
+    
+    @api.onchange('is_acumulado')
+    def onchange_is_acumulado(self):
+        if self.is_acumulado :
+            self.is_mensual = False
+            self.is_mensual_comp = False
+            self.is_acumulado_comp = False
+            self.is_saldo_ini = False
+    
+    @api.onchange('is_mensual_comp')
+    def onchange_is_mensual_comp(self):
+        if self.is_mensual_comp :
+            self.is_mensual = False
+            self.is_acumulado = False
+            self.is_acumulado_comp = False
+            self.is_saldo_ini = False
+            self.fecha_fin = self.fecha_fin_comp + relativedelta(years=-1)
+    
+    @api.onchange('is_acumulado_comp')
+    def onchange_is_acumulado_comp(self):
+        if self.is_acumulado_comp :
+            self.is_mensual = False
+            self.is_acumulado = False
+            self.is_mensual_comp = False
+            self.fecha_fin_acum = self.fecha_fin_comp_acum + relativedelta(years=-1)
+
+        # return {
+        #     'type': 'ir.actions.act_window',
+        #     'name': 'Reporte Balance',
+        #     'res_model': 'balance.record.group.dolar',
+        #     'view_type': 'form',
+        #     'view_id': False,
+        #     'view_mode': 'tree,form',
+        #     'target': 'current',
+        # }
+        
+        # data=self.env['account.move.line']
+
+        
+        # periodo=[('date','>=',self.fecha_ini),('date','<=',self.fecha_fin)]
+
+        # result = data.search(periodo)
+
+        # for rec in result:
+        #     rec.account_for_balance = 1
+
